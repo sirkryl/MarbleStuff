8d2773919076a174da92485b6ed43f5a6a3f969c
Kryl <chrisvhuber@gmail.com>
2016-03-27 23:37:08 +0200

    + initial

---
+++ Assets/FloorMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorMovement : MonoBehaviour {
+
+    public float rotationSpeed = 40f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(-Vector3.right * rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+    }
+}
